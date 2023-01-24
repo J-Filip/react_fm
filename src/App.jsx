ad5c331fe94c475b886f4344bac7b157b7a94c6f
J-Filip <filip.jugkala@yahoo.com>
2023-01-24 23:28:17 +0100

    Added jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,22 @@
 // flow of data very explicit - one way data flow  from parent to child (for the most part).
 // simply passing state to children using props - variables that a parent passes to its children!
 
-import React from "react"
-import {createRoot} from 'react-dom'
+// * I like using named exports for better maintainability and auto importing options in editors.
 
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me"),
-
-    React.createElement(Pet, {
-      name: "Kira",
-      animal: "Dog",
-      breed: "Schnauzer",
-    }),
-    React.createElement(Pet, {
-      name: "Lady",
-      animal: "Guinea pig",
-      breed: "Unknown",
-    }),
-    React.createElement(Pet, {
-      name: "Zeus",
-      animal: "Dog",
-      breed: "Turkish kangal",
-    }),
-  ]);
+  return (
+    <div>
+      <SearchParams />
+    </div>
+  );
 };
 let container = document.getElementById("root");
 
 // Then we take that element (which we called container) and pass that into ReactDOM.createRoot. This is how we signal to React where we want it to render our app.
 let root = createRoot(container);
 
-root.render(React.createElement(App));
+root.render(<App />);
