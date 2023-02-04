5fb80930e95626e1ca9be58ec47999acfcb9bd66
J-Filip <filip.jugkala@yahoo.com>
2023-02-05 00:01:46 +0100

    refactored useEffect with react query

--- src/hooks/useBreedList.js
+++ src/hooks/useBreedList.js
@@ -1,32 +1,36 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-const localCache = {};
+import { fetchBreedList } from "./fetchBreedList";
 
 export function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      getBreedList();
-    }
 
-    async function getBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      const request = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const data = await request.json();
-      localCache[animal] = data.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
+  // ! refactored useEffect with react query !
+  // useEffect(() => {
+  //   if (!animal) {
+  //     setBreedList([]);
+  //   } else if (localCache[animal]) {
+  //     setBreedList(localCache[animal]);
+  //   } else {
+  //     getBreedList();
+  //   }
 
-  return [breedList, status];
+  //   async function getBreedList() {
+  //     setBreedList([]);
+  //     setStatus("loading");
+  //     const request = await fetch(
+  //       `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  //     );
+  //     const data = await request.json();
+  //     localCache[animal] = data.breeds || [];
+  //     setBreedList(localCache[animal]);
+  //     setStatus("loaded");
+  //   }
+  // }, [animal]);
+  // !
+
+  // JS optional chaning - if results object doesnt have .data property (and so on), return undefined (instead of error). If not found, return empty array.
+  return [results?.data?.breeds ?? [], results.status];
 }
