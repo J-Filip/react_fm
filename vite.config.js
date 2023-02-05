1a6bb1d26db91e51e3e36d721bd4c379660026be
J-Filip <filip.jugkala@yahoo.com>
2023-02-05 16:41:24 +0100

    Defined build path to be in the root

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
       plugins: [react()],
       root: "src",
+      build: {
+        outDir: '../dist'
+      }
       
     });
