3ab2137005bf98b3b6e7b318a4ee6a4deea83f44
J-Filip <filip.jugkala@yahoo.com>
2023-02-05 22:26:04 +0100

    Added first context for the adopted pet

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@
 // * I like using named exports for better maintainability and auto importing options in editors.
 
 import React from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { SearchParams } from "./SearchParams";
 import { ControlledSearchParams } from "./ControlledSearchParams";
+import { AdoptedPetContext } from "./AdoptedPetContext";
 import { DetailsErrorBoundary as Details } from "./Details";
 
 const queryClient = new QueryClient({
@@ -28,11 +31,20 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+
+
+  // we are passing an entire hook as a value so we can use it's state and fucntion to change state in other components.
+const adoptedPet = useState(null)
+
+
+
   return (
-    
     // router and query client provider are just wrappers - they provide context to their children and they dont display anything!
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+
+      {/* You have to wrap your app in a Provider. This is the mechanism by which React will notify the higher components to re-render whenever our context changes. Then whatever you pass into the value prop (we passed in the complete hook, the value and updater pair) will exit on the other side whenever we ask for it. */}
+        <AdoptedPetContext.Provider value={adoptedPet}> 
         <Link to="/">
           <header>
             <h1>Adopt Me</h1>
@@ -43,6 +55,7 @@ const App = () => {
           <Route path="/" element={<SearchParams />} />
           <Route path="/details/:id" element={<Details />} />
         </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
